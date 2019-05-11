f454f32e74e2d093247dfd49789496cf824e4e85
litecoinfinance <litecoinfinance@gmail.com>
2019-05-11 03:44:23 +0300

    Fix send to BASE58 encoded address

--- chaincfg/params.go
+++ chaincfg/params.go
@@ -310,12 +310,12 @@ var MainNetParams = Params{
 
 	// Human-readable part for Bech32 encoded segwit addresses, as defined in
 	// BIP 173.
-	Bech32HRPSegwit: "bc", // always bc for main net
+	Bech32HRPSegwit: "ltfn", // ltfn for main net
 
 	// Address encoding magics
 	PubKeyHashAddrID:        0x1C, // starts with C sulition send to standart address LTFN ( lnd devs you forgor about this )
 	ScriptHashAddrID:        0x35, // starts with N solution send to standart address LTFN ( lnd devs you forgor about this )
-	PrivateKeyID:            0xB0, // starts with 5 (uncompressed) or K (compressed) no mater this to need modify ...
+	PrivateKeyID:            0xB0, // starts with 6 (uncompressed) or T (compressed)
 	WitnessPubKeyHashAddrID: 0x06, // starts with p2
 	WitnessScriptHashAddrID: 0x0A, // starts with 7Xh
 
